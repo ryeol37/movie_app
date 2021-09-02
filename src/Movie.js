36a8a95e8a2871f79afe9429382e3793acb4a43f
이동렬 <ryeol37@icloud.com>
2021-09-02 15:37:31 +0900

    영화 API로 본문 제작

---
+++ src/Movie.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({title, year, summary, poster, genres}) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="movie_genres">
+                    {genres.map((genres, index) => {
+                        return <li key={index} className="movie_genres">{genres}</li>
+                    })}
+                </ul>
+                <p className="movie_summary">{summary.slice(0, 180)}...</p>
+            </div>
+        </div>
+    );
+}
+
+Movie.prototype = {
+    title : PropTypes.string.isRequired,
+    year : PropTypes.number.isRequired,
+    summary : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
